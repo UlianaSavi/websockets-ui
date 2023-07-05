377d7ff1045d94374ac439509bd2e86756fd33e0
Uliana Savitskaua <uljasha.sav@gmail.com>
2023-07-05 15:29:29 +0600

    feat: init app & change scripts commands & add folders

---
+++ constants.ts
@@ -0,0 +1,1 @@
+export const HOSTNAME = '127.0.0.1';
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
+import dotenv from 'dotenv';
+import { HOSTNAME } from '../constants';
+
+
+dotenv.config();
+const port = process.env.SERVER_PORT ? +process.env.SERVER_PORT : 8181;
 
 export const httpServer = http.createServer(function (req, res) {
     const __dirname = path.resolve(path.dirname(''));
@@ -14,4 +20,9 @@ export const httpServer = http.createServer(function (req, res) {
         res.writeHead(200);
         res.end(data);
     });
+});
+
+
+httpServer.listen(port, HOSTNAME, () => {
+    console.log(`Server running at http://${ HOSTNAME }:${ port }/`);
 });
