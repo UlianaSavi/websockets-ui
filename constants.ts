18c88ba4d3e3668ec3d8058fd7c1f8d6e503b28d
Uliana Savitskaua <uljasha.sav@gmail.com>
2023-07-05 17:28:57 +0600

    feat: add websoket connection & add constants & delete trash

--- constants.ts
+++ constants.ts
@@ -1,1 +1,20 @@
 export const HOSTNAME = '127.0.0.1';
+
+export const BASE_COMMANDS = {
+    REG: 'reg',
+    UPDATE_WINS: 'update_winners',
+    CREATE_GAME: 'create_game',
+    ADD_SHIPS: 'add_ships',
+};
+export const ROOM_COMMANDS = {
+    CREATE_ROOM: 'create_room',
+    ADD_TO_ROOM: 'add_player_to_room',
+    UPDATE_ROOM: 'update_room',
+};
+
+export const GAME_COMMANDS = {
+    START_GAME: 'start_game',
+    FINISH: 'finish',
+    ATTACK: 'attack',
+    RAND_ATTACK: 'randomAttack',
+};
--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,12 @@ import * as path from 'path';
 import * as http from 'http';
 import dotenv from 'dotenv';
 import { HOSTNAME } from '../constants';
+import { WebSocketServer } from 'ws';
 
 dotenv.config();
-const port = process.env.SERVER_PORT ? +process.env.SERVER_PORT : 8181;
+const port = process.env.SERVER_PORT ? +process.env.SERVER_PORT : 3000;
 
-export const httpServer = http.createServer(function (req, res) {
+const httpServer = http.createServer((req, res) => {
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
     fs.readFile(file_path, function (err, data) {
@@ -21,7 +22,34 @@ export const httpServer = http.createServer(function (req, res) {
     });
 });
 
-
 httpServer.listen(port, HOSTNAME, () => {
     console.log(`Server running at http://${ HOSTNAME }:${ port }/`);
+});
+
+const wsServer = new WebSocketServer({
+    server: httpServer
+});
+
+wsServer.on('connection', function connection(ws, req) {
+
+    ws.on('error', (err) => {
+        console.error(err.message);
+    });
+
+    ws.on('message', function message(chunk) {
+        const req = JSON.parse(chunk.toString())
+        const command = req.type || 'unknown';
+
+        switch (command) {
+            case 'reg':
+                console.log('This is reg');
+                break;
+            case 'update_winners':
+                console.log('This is reg');
+                break;
+        
+            default:
+                break;
+        }
+    });
 });
